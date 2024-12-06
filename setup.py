b94c9d3943d47c18cba9b87355ee437f9e22ad7b
munir-v <81745551+munir-v@users.noreply.github.com>
2024-12-05 19:57:47 -0800

    working: simplify setup.py by removing unnecessary metadata and comments

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
-# setup.py
 from setuptools import setup
 
-APP = ['app.py']  # Ensure this matches your main script's name
+APP = ['app.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
-        'LSUIElement': True,  # Makes the app a background-only app (no Dock icon)
-        'CFBundleName': 'StopwatchApp',
-        'CFBundleShortVersionString': '0.1.0',
-        'CFBundleVersion': '0.1.0',
-        'CFBundleIdentifier': 'com.yourname.stopwatchapp',  # Replace with your unique identifier
+        'LSUIElement': True,
     },
-    'packages': ['rumps', 'openpyxl', 'Cocoa', 'AppKit'],
-    # 'iconfile': 'app.icns',  # Optional: specify your app icon
+    'packages': ['rumps','Cocoa', 'AppKit'],
 }
 
 setup(
