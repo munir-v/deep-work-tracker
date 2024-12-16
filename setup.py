3a6c8d74df2f5125ff4eb6fbd387d35a2af4846c
munir-v <81745551+munir-v@users.noreply.github.com>
2024-12-15 19:33:13 -0800

    chore: update .gitignore and modify setup.py for improved packaging

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup
 APP = ['app.py']
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
+    'argv_emulation': False,
     'plist': {
-        'LSUIElement': True,
+        'LSUIElement': True,  # Hides the dock icon, keeping the app in the menu bar
+        'CFBundleName': 'StopwatchApp',
+        'CFBundleShortVersionString': '1.0',
+        'CFBundleVersion': '1.0',
+        'NSPrincipalClass': 'NSApplication',
     },
-    'packages': ['rumps','Cocoa', 'AppKit'],
+    'packages': ['rumps', 'AppKit', 'Cocoa'],  # Includes necessary dependencies
+    'includes': ['json', 'os', 'sys', 'datetime', 'functools', 'pathlib'],  # Ensure included modules
+    'excludes': ['tkinter', 'unittest','Carbon'],  # Exclude unused modules to reduce bundle size
 }
 
 setup(
