010c8cfbcdc2e4c78bf7d26d5bb996c0eb95833b
munir-v <81745551+munir-v@users.noreply.github.com>
2024-12-16 07:44:55 -0800

    chore: update setup.py to change app name and modify included packages

--- setup.py
+++ setup.py
@@ -6,17 +6,13 @@ OPTIONS = {
     'argv_emulation': False,
     'plist': {
         'LSUIElement': True,  # Hides the dock icon, keeping the app in the menu bar
-        'CFBundleName': 'StopwatchApp',
-        'CFBundleShortVersionString': '1.0',
-        'CFBundleVersion': '1.0',
-        'NSPrincipalClass': 'NSApplication',
     },
     'packages': ['rumps', 'AppKit', 'Cocoa'],  # Includes necessary dependencies
-    'includes': ['json', 'os', 'sys', 'datetime', 'functools', 'pathlib'],  # Ensure included modules
-    'excludes': ['tkinter', 'unittest','Carbon'],  # Exclude unused modules to reduce bundle size
+    'includes': ['imp'],
 }
 
 setup(
+    name='Deep Work Tracker',
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
