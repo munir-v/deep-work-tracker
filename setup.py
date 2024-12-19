955b5fe032a453cf48357b3b98cd149de623c68f
munir-v <81745551+munir-v@users.noreply.github.com>
2024-12-18 17:01:00 -0800

    fix: standardize string quotes in setup.py for consistency

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
-APP = ['app.py']
+APP = ["app.py"]
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': False,
-    'plist': {
-        'LSUIElement': True, 
+    "argv_emulation": False,
+    "plist": {
+        "LSUIElement": True,
     },
-    'packages': ['rumps', 'AppKit', 'Cocoa'], 
-    'includes': ['imp'],
-    'iconfile': 'icons/icon.icns',
+    "packages": ["rumps", "AppKit", "Cocoa"],
+    "includes": ["imp"],
+    "iconfile": "icons/icon.icns",
 }
 
 setup(
-    name='Deep Work Tracker',
+    name="Deep Work Tracker",
     app=APP,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
